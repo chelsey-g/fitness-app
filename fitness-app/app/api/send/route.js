8e6b439265525edfeb4c79593da4c2df6924dfdb
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2024-05-28 16:05:29 -0400

    ts to jsx

---
+++ fitness-app/app/api/send/route.js
@@ -0,0 +1,48 @@
+"use server"
+
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export const sendEmail = async (formData) => {
+  const name = formData.get("name")
+  const email = formData.get("email")
+  const message = formData.get("message")
+
+  if (
+    !name ||
+    !email ||
+    !message ||
+    typeof name !== "string" ||
+    typeof email !== "string" ||
+    typeof message !== "string"
+  ) {
+    throw new Error("Invalid input")
+  }
+
+  await resend.emails.send({
+    from: "Contact <onboarding@resend.dev>",
+    to: "chelgowac@gmail.com",
+    subject: "Message from Contact Form",
+    reply_to: email,
+    text: message,
+  })
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const formData = new FormData()
+      formData.append("name", req.body.name)
+      formData.append("email", req.body.email)
+      formData.append("message", req.body.message)
+
+      await sendEmail(formData)
+      res.status(200).json({ message: "Email sent successfully" })
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" })
+  }
+}
