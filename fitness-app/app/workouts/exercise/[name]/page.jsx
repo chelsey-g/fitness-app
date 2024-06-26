ed40fac3f5452f61be0c99e8cf76346937cd6270
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2024-06-26 16:24:14 -0400

    layout is weird for workouts, needs a ui update #39

--- fitness-app/app/workouts/exercise/[name]/page.jsx
+++ fitness-app/app/workouts/exercise/[name]/page.jsx
@@ -64,39 +64,45 @@ export default function ExercisePage(props) {
   return (
     <>
       <Navigation />
-      <div className="w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow-lg">
-        <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/2">
-            <h1 className="text-sm font-semibold text-gray-800 mb-2">
-              {exerciseData[0]?.name} ({exerciseData[0]?.difficulty})
-            </h1>
-            <div className="mb-4">
-              <h2 className="text-xs font-medium text-gray-700">
-                Equipment required:{" "}
-                <span className="text-gray-600">
-                  {exerciseData[0]?.equipment}
-                </span>
-              </h2>
+      <div className="container mx-auto p-6 rounded-lg container mx-auto m-4 p-4 max-w-xl">
+        <div className="bg-white border border-gray-200 rounded-lg shadow-lg p-6">
+          <BackButton />
+          <div className="flex flex-col md:flex-row">
+            <div className="md:w-1/2">
+              <h1 className="text-sm font-semibold text-gray-800 mb-2">
+                {exerciseData[0]?.name} ({exerciseData[0]?.difficulty})
+              </h1>
+              <div className="mb-4">
+                <h2 className="text-xs font-medium text-gray-700">
+                  Equipment required:{" "}
+                  <span className="text-gray-600">
+                    {exerciseData[0]?.equipment}
+                  </span>
+                </h2>
+              </div>
+              <div className="mb-4">
+                <h2 className="text-xs font-medium text-gray-700">
+                  Primary Muscle Groups:{" "}
+                  <span className="text-gray-600">
+                    {exerciseData[0]?.muscle}
+                  </span>
+                </h2>
+              </div>
             </div>
-            <div className="mb-4">
-              <h2 className="text-xs font-medium text-gray-700">
-                Primary Muscle Groups:{" "}
-                <span className="text-gray-600">{exerciseData[0]?.muscle}</span>
-              </h2>
+            <div className="md:w-1/2 text-center">
+              <p className="text-gray-600">{exerciseData[0]?.instructions}</p>
             </div>
           </div>
-          <div className="md:w-1/2 text-center">
-            <p className="text-gray-600">{exerciseData[0]?.instructions}</p>
+          <div className="flex justify-center mt-5">
+            <WorkOutModal
+              className="py-2 px-4 bg-snd-bkg hover:bg-snd-bkg-hover text-white rounded-md focus:outline-none text-center"
+              isWorkOutModalVisible={isWorkOutModalVisible}
+              handleOkButton={handleOkButton}
+              handleAddWorkout={handleAddWorkout}
+              exerciseData={exerciseData}
+            />
           </div>
         </div>
-        <WorkOutModal
-          className="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md focus:outline-none mt-5 text-center"
-          isWorkOutModalVisible={isWorkOutModalVisible}
-          handleOkButton={handleOkButton}
-          handleAddWorkout={handleAddWorkout}
-          exerciseData={exerciseData}
-        />
-        <BackButton />
       </div>
     </>
   )
