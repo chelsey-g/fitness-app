50f36a3fac350959cde4a5b925de8ad01ea7c539
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2024-07-03 12:34:54 -0400

    #50 add clear button for search results

--- fitness-app/components/Search.jsx
+++ fitness-app/components/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 
 import DifficultyFilter from "@/components/DifficultyFilter"
+import { IoIosClose } from "react-icons/io"
 import { useRouter } from "next/navigation"
 
 export default function SearchBar({ onResultsChange }) {
@@ -67,25 +68,37 @@ export default function SearchBar({ onResultsChange }) {
     setType(type)
   }
 
+  const handleClearSearch = () => {
+    setSearchValue("")
+    setResults([])
+  }
+
   return (
-    <div className="p-4 rounded-lg ">
-      <div className="mb-4 text-center">
-        <input
-          className="w-3/4
-         p-2 border border-gray-300 rounded-md text-center text-black hover:border-gray-500 focus:outline-none focus:border-snd-bkg  focus:ring-white"
-          type="text"
-          placeholder="Search Exercise"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <button
-          className="py-2 px-4 bg-snd-bkg text-white rounded-md focus:outline-none ml-2 "
-          onClick={() => {
-            handleSearch()
-          }}
-        >
-          Search
-        </button>
+    <div className="p-4 rounded-lg">
+      <div className="mb-4 text-center relative">
+        <div className="flex justify-center items-center relative w-full sm:w-3/4 mx-auto">
+          <input
+            className="w-full p-2 border border-gray-300 rounded-md text-center text-black hover:border-gray-500 focus:outline-none focus:border-snd-bkg focus:ring-white"
+            type="text"
+            placeholder="Search Exercise"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          {results.length > 0 && (
+            <button
+              className="absolute inset-y-0 right-20 flex items-center pr-3 text-gray-500"
+              onClick={handleClearSearch}
+            >
+              <IoIosClose size={24} />
+            </button>
+          )}
+          <button
+            className="ml-2 py-2 px-4 bg-snd-bkg text-white rounded-md focus:outline-none"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
         {results.length > 0 && (
           <DifficultyFilter
             difficulty={difficulty}
@@ -96,10 +109,8 @@ export default function SearchBar({ onResultsChange }) {
           {results.map((result, index) => (
             <div
               key={index}
-              className="bg-white shadow-md p-4 mb-4 rounded-lg mt-5"
-              onClick={() => {
-                handleSearchClick(result)
-              }}
+              className="bg-white shadow-md p-4 mb-4 rounded-lg mt-5 cursor-pointer"
+              onClick={() => handleSearchClick(result)}
             >
               <h2 className="text-2xl font-bold text-gray-800">
                 {result.name}
