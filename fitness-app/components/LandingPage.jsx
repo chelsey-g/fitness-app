0977d2c5fbc7feb0f69bf4ed1eef5e31825252aa
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2024-07-19 10:08:03 -0400

    landing page : margins fixed for tools & text-logo

--- fitness-app/components/LandingPage.jsx
+++ fitness-app/components/LandingPage.jsx
@@ -29,7 +29,7 @@ export default function LandingPage() {
         />
         <div className="relative z-20 text-center mt-20 mb-10">
           <Image
-            className="mx-auto mb-5 opacity-80"
+            className="mx-auto flex mt-10 opacity-80"
             width={300}
             height={150}
             src="/images/text-logo.png"
@@ -38,12 +38,12 @@ export default function LandingPage() {
           <h2 className="text-4xl text-white font-bold mb-4">
             Fitness At Your Fingertips
           </h2>
-          <p className="text-lg text-white mb-6 px-4 italic mb-10">
+          <p className="text-lg text-white mb-6 px-4 italic mb-20">
             Your path to friendly competition, motivation, and a healthier
             lifestyle.
           </p>
           <button
-            className="bg-trd-bkg hover:bg-green-700 text-white font-bold py-6 px-12 text-2xl rounded-lg shadow transition duration-300 ease-in-out mt-20"
+            className="bg-trd-bkg hover:bg-green-700 text-white font-bold py-6 px-12 text-2xl rounded-lg shadow transition duration-300 ease-in-out"
             onClick={handleJoinNowButton}
           >
             Get Started
@@ -57,7 +57,7 @@ export default function LandingPage() {
             Explore Our Tools
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white ml-4">
+            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white">
               <div className="flex justify-center mb-4">
                 <GiBodyHeight className="text-4xl text-blue-500 mx-2" />
                 <FaCalculator className="text-4xl text-green-500 mx-2" />
@@ -69,21 +69,21 @@ export default function LandingPage() {
               </p>
             </div>
 
-            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white ml-4">
+            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white">
               <FaBullseye className="text-4xl text-red-500 mx-auto mb-4" />
               <h3 className="text-xl font-bold">Start a Goal</h3>
               <p className="text-gray-600 mt-2">
                 Set and track your personal fitness goals.
               </p>
             </div>
-            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white ml-4">
+            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white">
               <FaTrophy className="text-4xl text-yellow-500 mx-auto mb-4" />
               <h3 className="text-xl font-bold">Create a Competition</h3>
               <p className="text-gray-600 mt-2">
                 Challenge your friends and stay motivated together.
               </p>
             </div>
-            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white ml-4">
+            <div className="testimonial max-w-md text-center p-4 border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out bg-white">
               <FaRunning className="text-4xl text-red-500 mx-auto mb-4" />
               <h3 className="text-xl font-bold">Personalized Workouts</h3>
               <p className="text-gray-600 mt-2">
@@ -100,7 +100,7 @@ export default function LandingPage() {
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center">
             <div className="bg-white shadow-md rounded-lg p-6 text-center hover:shadow-lg transition duration-300 ease-in-out cursor-pointer h-48 flex flex-col justify-center">
-              <FaQuoteLeft className="text-4xl text-blue-500 mx-auto mb-4" />
+              <FaQuoteLeft className="text-3xl text-blue-500 mx-auto mb-4" />
               <p className="italic mb-2">
                 "This app has completely revolutionized my approach to fitness
                 and challenges!"
@@ -108,7 +108,7 @@ export default function LandingPage() {
               <p className="font-bold">- Jane Doe</p>
             </div>
             <div className="bg-white shadow-md rounded-lg p-6 text-center hover:shadow-lg transition duration-300 ease-in-out cursor-pointer h-48 flex flex-col justify-center">
-              <FaQuoteLeft className="text-4xl text-green-500 mx-auto mb-4" />
+              <FaQuoteLeft className="text-3xl text-green-500 mx-auto mb-4" />
               <p className="italic mb-2">
                 "I've never been more inspired to stay active and reach my
                 goals!"
@@ -116,7 +116,7 @@ export default function LandingPage() {
               <p className="font-bold">- John Smith</p>
             </div>
             <div className="bg-white shadow-md rounded-lg p-6 text-center hover:shadow-lg transition duration-300 ease-in-out cursor-pointer h-48 flex flex-col justify-center">
-              <FaQuoteLeft className="text-4xl text-red-500 mx-auto mb-4" />
+              <FaQuoteLeft className="text-3xl text-red-500 mx-auto mb-4" />
               <p className="italic mb-2">
                 "The friendly competition keeps me motivated and striving for
                 more every day."
