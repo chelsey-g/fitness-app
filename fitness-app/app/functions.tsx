a2f6e2d9169fc2027df5e7305ad10c1867856008
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2024-08-29 12:53:14 -0400

    functions page typescript conversion

--- fitness-app/app/functions.tsx
+++ fitness-app/app/functions.tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs"
 
-export function getAwardColor(position) {
+export function getAwardColor(position: number) {
   if (position === 1) {
     return "text-yellow-500"
   } else if (position === 2) {
@@ -10,7 +10,7 @@ export function getAwardColor(position) {
   }
 }
 
-export function getOrdinalSuffix(number) {
+export function getOrdinalSuffix(number: number) {
   const suffixes = ["th", "st", "nd", "rd"]
   const v = number % 100
   return number + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0])
@@ -21,16 +21,19 @@ export function getRandomColor() {
   return colors[Math.floor(Math.random() * colors.length)]
 }
 
-export function handleDate(date) {
+export function handleDate(date: any) {
   return dayjs(date).format("MM/DD/YYYY")
 }
 
-export const calculateWeightDifference = (goalWeight, currentWeight) => {
+export const calculateWeightDifference = (
+  goalWeight: number,
+  currentWeight: number
+) => {
   const difference = goalWeight - currentWeight
   return difference.toFixed(2)
 }
 
-export const calculateDaysLeft = (date) => {
+export const calculateDaysLeft = (date: any) => {
   const goalDate = new Date(date)
   const today = new Date()
   const differenceInTime = goalDate.getTime() - today.getTime()
