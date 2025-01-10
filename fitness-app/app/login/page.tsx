2d0f17d61e5890b4c992fa57bcacd58ce3a1a228
chelsey-g <97612806+chelsey-g@users.noreply.github.com>
2025-01-10 09:55:57 -0500

    fix: updated login layout #76

--- fitness-app/app/login/page.tsx
+++ fitness-app/app/login/page.tsx
@@ -1,28 +1,28 @@
-import Image from "next/image"
 import Messages from "./messages"
-import Navigation from "@/components/Navigation"
 import { Suspense } from "react"
+
+import { IoIosLock } from "react-icons/io"
+import { FcGoogle } from "react-icons/fc"
+
+import Navigation from "@/components/Navigation"
+import Footer from "@/components/Footer"
 
 export default function Login() {
   return (
     <div>
       <Suspense fallback={<div>Loading...</div>}>
         <Navigation />
-        <div className="w-full px-8 py-4 sm:max-w-md mx-auto">
+        <div className="px-2 py-4 sm:max-w-lg mx-auto">
           <form
-            className="bg-prm-bkg p-4 rounded-lg flex flex-col gap-2 text-foreground"
+            className="p-4 rounded-lg flex flex-col gap-2 text-foreground"
             action="/auth/sign-in"
             method="post"
           >
-            <div className="flex justify-center mb-4">
-              <Image
-                src="/images/text-logo.png"
-                alt="HabitKick Logo"
-                width={200}
-                height={200}
-                className="items-center"
-              />
+            <div className="flex items-center space-x-2 mb-4">
+              <h1 className="text-2xl font-semibold">Log in to your account</h1>
+              <IoIosLock className="text-2xl text-logo-green" />
             </div>
+
             <Messages />
             <label className="text-md" htmlFor="email">
               Email
@@ -37,24 +37,36 @@ export default function Login() {
               Password
             </label>
             <input
-              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              className="rounded-md px-4 py-2 bg-inherit border mb-3"
               type="password"
               name="password"
               placeholder="••••••••"
               required
             />
-            <button className="bg-snd-bkg rounded-md px-4 py-2 text-foreground mb-2">
-              Sign In
+            <div className="text-right mb-3">
+              <a
+                href="/login/help"
+                className="text-sm text-logo-green hover:underline"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <button className="bg-logo-green hover:opacity-90 rounded-md px-4 py-2 mb-2 dark:bg-snd-bkg">
+              Log in
             </button>
-            <button
-              formAction="/auth/sign-up"
-              className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 bg-trd-bkg hover:bg-trd-bkg-hover"
-            >
-              Sign Up
+            <div className="flex items-center justify-center space-x-3 text-white-300 my-2">
+              <div className="flex-grow border-t border-gray-600"></div>
+              <span className="px-2 text-sm uppercase text-gray-400">Or</span>
+              <div className="flex-grow border-t border-gray-600"></div>
+            </div>
+            <button className="w-full flex items-center justify-center gap-2 bg-cream-white hover:opacity-90 dark:bg-black rounded-md px-4 py-2 mt-2 border border-dashed border-logo-green">
+              <FcGoogle />
+              Continue With Google
             </button>
           </form>
         </div>
       </Suspense>
+      <Footer />
     </div>
   )
 }
